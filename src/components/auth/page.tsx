6e65c48fc2df72be88b829b454d6e6f36ff0873a
Shirin Mohajer <shirin@Shirins-MacBook-Pro.local>
2024-08-20 14:00:01 -0400

    added logos for logins

--- src/components/auth/page.tsx
+++ src/components/auth/page.tsx
@@ -1,24 +1,38 @@
 import AuthSocialButtons from "../common/AuthSocialButtons";
-import { signIn } from "../../app/auth";
+import googleLogo from "../../assets/google-logo.png";
+import githubLogo from "../../assets/github-logo.png";
+import microsoftLogo from "../../assets/microsoft-logo.png";
+
 export default function SignIn() {
   return (
-    <form
-    // action={async () => {
-    //   "use server";
-    //   await signIn("google");
-    // }}
-    >
-      <div className="flex justify-center border-2 border-black gap-2 p-20 m-auto">
+    <div className="flex justify-center rounded-2xl border-2 border-black gap-2 p-20 m-auto">
+      <div className="flex flex-col">
+        <img
+          className="rounded-2xl border-2"
+          src="https://beforeigosolutions.com/wp-content/uploads/2021/12/dummy-profile-pic-300x300-1.png"
+        />
         <div className="my-5">
-          <AuthSocialButtons loginTo="google" title="Google" />
+          <AuthSocialButtons
+            loginTo="google"
+            title="Google"
+            socialLogo={googleLogo.src}
+          />
         </div>
         <div className="my-5">
-          <AuthSocialButtons loginTo="github" title="GitHub" />
+          <AuthSocialButtons
+            loginTo="github"
+            title="GitHub"
+            socialLogo={githubLogo.src}
+          />
         </div>
         <div className="my-5">
-          <AuthSocialButtons loginTo="microsoft" title="Microsoft" />
+          <AuthSocialButtons
+            loginTo="microsoft"
+            title="Microsoft"
+            socialLogo={microsoftLogo.src}
+          />
         </div>
       </div>
-    </form>
+    </div>
   );
 }
