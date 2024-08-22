a4dbec17f7bfee5fd2615815f0a3f18aa330a443
Shirin Mohajer <shirin@Shirins-MacBook-Pro.local>
2024-08-22 15:28:48 -0400

    about this app update

--- src/components/about/AboutApp.tsx
+++ src/components/about/AboutApp.tsx
@@ -1,24 +1,20 @@
 const AboutApp = () => {
   return (
-    <div className="py-10 px-20">
+    <div className="py-10 px-50 mx-auto">
       <h1 className="text-xl my-10">About This App</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero corrupti
-        ipsum animi ratione in repellat recusandae odit eligendi tempora. Dolor
-        maiores soluta reiciendis dolores voluptas ab commodi nihil rerum
-        suscipit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
-        corrupti ipsum animi ratione in repellat recusandae odit eligendi
-        tempora. Dolor maiores soluta reiciendis dolores voluptas ab commodi
-        nihil rerum suscipit. Lorem ipsum dolor sit amet consectetur,
-        adipisicing elit. Vero corrupti ipsum animi ratione in repellat
-        recusandae odit eligendi tempora. Dolor maiores soluta reiciendis
-        dolores voluptas ab commodi nihil rerum suscipit. Lorem ipsum dolor sit
-        amet consectetur, adipisicing elit. Vero corrupti ipsum animi ratione in
-        repellat recusandae odit eligendi tempora. Dolor maiores soluta
-        reiciendis dolores voluptas ab commodi nihil rerum suscipit. Lorem ipsum
-        dolor sit amet consectetur, adipisicing elit. Vero corrupti ipsum animi
-        ratione in repellat recusandae odit eligendi tempora. Dolor maiores
-        soluta reiciendis dolores voluptas ab commodi nihil rerum suscipit.
+        "**Secure Note App** is designed to keep your notes safe and accessible.
+        Whether you're jotting down personal thoughts or storing important
+        information, our app ensures that your data is protected with top-notch
+        encryption. You can log in securely using your Google, GitHub, or
+        Microsoft account through Single Sign-On (SSO) or opt for a traditional
+        username and password.
+      </p>
+      <p>
+        With a focus on user security and convenience, Secure Note App offers a
+        seamless experience across all your devices. Your notes are always
+        within reach, yet completely secure, providing peace of mind that your
+        information is protected against unauthorized access."
       </p>
     </div>
   );
