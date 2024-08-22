595315975479bf916f46a84f3aaf6204468f4a6d
Shirin Mohajer <shirin@Shirins-MacBook-Pro.local>
2024-08-22 11:14:37 -0400

    added sign in form

--- src/components/auth/page.tsx
+++ src/components/auth/page.tsx
@@ -2,35 +2,43 @@ import AuthSocialButtons from "../common/AuthSocialButtons";
 import googleLogo from "../../assets/google-logo.png";
 import githubLogo from "../../assets/github-logo.png";
 import microsoftLogo from "../../assets/microsoft-logo.png";
+import SignInForm from "./login/SignInForm";
 
 export default function SignIn() {
   return (
-    <div className="flex justify-center rounded-2xl border-2 border-black gap-2 p-20 m-auto">
-      <div className="flex flex-col">
-        <img
-          className="rounded-2xl border-2"
-          src="https://beforeigosolutions.com/wp-content/uploads/2021/12/dummy-profile-pic-300x300-1.png"
-        />
-        <div className="my-5">
-          <AuthSocialButtons
-            loginTo="google"
-            title="Google"
-            socialLogo={googleLogo.src}
-          />
+    <div className="flex rounded-2xl border-2 border-black p-20 my-20">
+      <div className="flex flex-col md:flex-row justify-center  ">
+        <div className="flex flex-col  ml-0 gap-4 md:border-r-4 px-6">
+          <div>
+            <img
+              className="flex rounded-2xl border-2 w-40"
+              src="https://beforeigosolutions.com/wp-content/uploads/2021/12/dummy-profile-pic-300x300-1.png"
+            />
+          </div>
+          <div className="flex my-2  ">
+            <AuthSocialButtons
+              loginTo="google"
+              title="Google"
+              socialLogo={googleLogo.src}
+            />
+          </div>
+          <div className="flex my-2  ">
+            <AuthSocialButtons
+              loginTo="github"
+              title="GitHub"
+              socialLogo={githubLogo.src}
+            />
+          </div>
+          <div className="my-2  ">
+            <AuthSocialButtons
+              loginTo="microsoft"
+              title="Microsoft"
+              socialLogo={microsoftLogo.src}
+            />
+          </div>
         </div>
-        <div className="my-5">
-          <AuthSocialButtons
-            loginTo="github"
-            title="GitHub"
-            socialLogo={githubLogo.src}
-          />
-        </div>
-        <div className="my-5">
-          <AuthSocialButtons
-            loginTo="microsoft"
-            title="Microsoft"
-            socialLogo={microsoftLogo.src}
-          />
+        <div className="px-2">
+          <SignInForm />
         </div>
       </div>
     </div>
